fe1bcf696e9a4a936fd5b94347558e766d6d2902
4332001876 <mzfslhm@163.com>
2023-12-08 16:50:21 +0800

    elastic_search_helper

--- src/data_access/elastic_search_helper.py
+++ src/data_access/elastic_search_helper.py
@@ -1,11 +1,57 @@
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(
-    [
-        {"host": "host1-ip-address", "port": port-number}, 
-        {"host": "host2-ip-address", "port": port-number},
-        {"host": "host3-ip-address", "port": port-number}
-    ],
-    http_auth=("username", "secret"), 
-    timeout=3600
-)
+from data_access.file_record import FileRecord
+
+class ElasticsearchHelper:
+    def __init__(self) -> None:  
+        # connect
+        self.es = Elasticsearch(host="0.0.0.0",port=9200,timeout=60)
+
+    def insert(self, rowkey, file_record: FileRecord):
+        # insert
+        data = {
+            "rowkey":rowkey,
+            "title":file_record.title
+        }
+        self.es.index(index="test",doc_type="all_type",body=data)
+
+    def query(self, keyword):
+        # query
+        query = {
+            "query":{
+                "term":{
+                    "title":keyword
+                }
+            }
+        }
+        results = self.es.search(index="test",doc_type="all_type",body=query)
+        rowkeys = [result['_source']["rowkey"] for result in results['hits']['hits']]
+        return rowkeys
+    
+    def rowkey_to_id(self, rowkey):
+        query = {
+            "query":{
+                "term":{
+                    "rowkey":rowkey
+                }
+            }
+        }
+        results = self.es.search(index="test",doc_type="all_type",body=query)
+        ids = [result['_id'] for result in results['hits']['hits']]
+        return ids
+
+    def delete(self, rowkey):
+        ids = self.rowkey_to_id(rowkey)
+        for id in ids:
+            # delete
+            self.es.delete(index="test",doc_type="all_type",id=id)
+
+    def update(self, rowkey, file_record: FileRecord):
+        ids = self.rowkey_to_id(rowkey)
+        for id in ids:
+            # update
+            data = {
+                "rowkey":rowkey,
+                "title":file_record.title
+            }
+            self.es.update(index="test",doc_type="all_type",id=id,body=data)
--- src/data_access/hbase_helper.py
+++ src/data_access/hbase_helper.py
@@ -1,28 +1,7 @@
 import happybase
 from config import Config
 
-class FileRecord:
-    def __init__(self, title, url, time, source, file_type, file_type2) -> None:
-        self.title = title
-        self.url = url
-        self.time = time
-        self.source = source
-        self.file_type = file_type
-        self.file_type2 = file_type2
-
-    def __str__(self) -> str:
-        return f'{self.title},{self.url},{self.time},{self.source},{self.file_type},{self.file_type2}'
-
-    def get_hbase_data_format(self):
-        return {
-            b'info:title': self.title.encode(), # "string".encode() -> b"string" (bytes), 
-            b'info:url': self.url.encode(),
-            b'info:time': self.time.encode(),
-            b'info:source': self.source.encode(),
-            b'info:file_type': self.file_type.encode(),
-            b'info:file_type2': self.file_type2.encode()
-        }
-
+from data_access.file_record import FileRecord
 
 class HbaseHelper:
     def __init__(self) -> None:
